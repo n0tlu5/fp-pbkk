d8fd0dc1054c2e50fbe807c4b4659a4a5e815671
ydstiroa <47847548+ydstiroa@users.noreply.github.com>
2020-05-16 11:44:37 +0700

    update

--- application/views/Admin/kebutuhan/index.php
+++ application/views/Admin/kebutuhan/index.php
@@ -26,7 +26,7 @@
 					<!-- DataTables -->
 					<div class="card mb-3">
 						<div class="card-header">
-							<a href="<?php echo site_url('admin/products/add') ?>"><i class="fas fa-plus"></i> Add New</a>
+							<a href="<?php echo site_url('admin/kebutuhan/add') ?>"><i class="fas fa-plus"></i> Add New</a>
 						</div>
 						<div class="card-body">
 
@@ -34,34 +34,37 @@
 								<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
 									<thead>
 										<tr>
-											<th>Name</th>
-											<th>Category</th>
-											<th>Price</th>
-											<th>Description</th>
-											<th>Action</th>
+											<th>Nama</th>
+											<th>Jenis</th>
+											<th>Jumlah</th>
+											<th>Satuan</th>
+											<th>Keterangan</th>
 										</tr>
 									</thead>
 									<tbody>
+										<?php foreach ($kebutuhan as $kebutuhan): ?>
 										<tr>
 											<td width="150">
-												name
-											</td>
-											<td width="150">
-												category_name
+												<?php echo $kebutuhan->nama ?>
 											</td>
 											<td>
-												price
+												<?php echo $kebutuhan->jenis ?>
 											</td>
-											<td class="small">
-												harga
+											<td>
+												<?php echo $kebutuhan->jumlah ?>
+											</td>
+											<td>
+												<?php echo $kebutuhan->keterangan ?>
+											</td>
 											<td width="250">
-												<a href="<?php echo site_url('admin/products/edit/1') ?>"
+												<a href="<?php echo site_url('admin/kebutuhan/edit/'.$kebutuhan->kebutuhan_id) ?>"
 												class="btn btn-small"><i class="fas fa-edit"></i> Edit</a>
-												<a onclick="deleteConfirm(test)"
-												href="#!" class="btn btn-small text-danger"><i class="fas fa-trash"></i> Hapus</a>
+												<a onclick="deleteConfirm('<?php echo site_url('admin/kebutuhan/delete/'.$kebutuhan->kebutuhan_id) ?>')"
+												 href="#!" class="btn btn-small text-danger"><i class="fas fa-trash"></i> Hapus</a>
 											</td>
 										</tr>
-
+										<?php endforeach; ?>
+										
 									</tbody>
 								</table>
 							</div>
---
+++ application/views/Admin/kebutuhan/new_form.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view("admin/_partials/head.php") ?>
+</head>
+
+<body id="page-top">
+	<!-- Page Wrapper -->
+	<div id="wrapper">
+		<?php $this->load->view("admin/_partials/sidebar.php") ?>
+	<!-- Content Wrapper -->
+		<div id="content-wrapper" class="d-flex flex-column">
+
+			<!-- Main Content -->
+			<div id="content">
+				<?php $this->load->view("admin/_partials/navbar.php") ?>
+				<!-- Begin Page Content -->
+				<div class="container-fluid">
+					<!-- Page Heading -->
+					<div class="d-sm-flex align-items-center justify-content-between mb-4">
+						<h1 class="h3 mb-0 text-gray-800">Add product</h1>
+					</div>
+
+					<?php if ($this->session->flashdata('success')): ?>
+					<div class="alert alert-success" role="alert">
+						<?php echo $this->session->flashdata('success'); ?>
+					</div>
+					<?php endif; ?>
+
+					<div class="card mb-3">
+						<div class="card-header">
+							<a href="<?php echo site_url('dashboard/donasi/') ?>"><i class="fas fa-arrow-left"></i> Back</a>
+						</div>
+						<div class="card-body">
+
+							<form action="<?php base_url('dashboard/kebutuhan/add') ?>" method="post" enctype="multipart/form-data" >
+
+								<div class="form-group">
+									<label for="nama">Nama*</label>
+									<input class="form-control <?php echo form_error('nama') ? 'is-invalid':'' ?>"
+									type="text" name="nama" placeholder="Nama kebutuhan" />
+									<div class="invalid-feedback">
+										<?php echo form_error('nama') ?>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="jumlah">Jumlah*</label>
+									<input class="form-control <?php echo form_error('jumlah') ? 'is-invalid':'' ?>"
+									type="number" name="jumlah" min="0" placeholder="Jumlah kebutuhan" />
+									<div class="invalid-feedback">
+										<?php echo form_error('jumlah') ?>
+									</div>
+								</div>
+
+
+								<div class="form-group">
+									<label for="satuan">Satuan</label>
+									<input class="form-control-file <?php echo form_error('satuan') ? 'is-invalid':'' ?>"
+									type="file" name="satuan" />
+									<div class="invalid-feedback">
+										<?php echo form_error('satuan') ?>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="keterangan">Keterangan*</label>
+									<textarea class="form-control <?php echo form_error('keterangan') ? 'is-invalid':'' ?>"
+									name="keterangan" placeholder="Keterangan kebutuhan..."></textarea>
+									<div class="invalid-feedback">
+										<?php echo form_error('keterangan') ?>
+									</div>
+								</div>
+
+								<input class="btn btn-success" type="submit" name="btn" value="Save" />
+							</form>
+
+						</div>
+
+						<div class="card-footer small text-muted">
+							* required fields
+						</div>
+
+
+					</div>
+
+				</div>
+			</div>
+			<?php $this->load->view("admin/_partials/footer.php") ?>
+		</div>
+	</div>
+	<?php $this->load->view("admin/_partials/scrolltop.php") ?>
+	<?php $this->load->view("admin/_partials/modal.php") ?>
+	<?php $this->load->view("admin/_partials/js.php") ?>
+	
+</body>
