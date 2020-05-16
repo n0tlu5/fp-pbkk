401d47ce97a2ff4298ac9b2b525819c8a7e93dc4
maybe <darfisultoni@gmail.com>
2020-05-16 17:50:19 +0700

    salah nama class

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Product_model extends CI_Model
+class User_model extends CI_Model
 {
     private $_table = "products";
 
