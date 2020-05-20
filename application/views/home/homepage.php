2542d2f12b20b51bb31180884c63041ed4e19474
ydstiroa <47847548+ydstiroa@users.noreply.github.com>
2020-05-20 16:43:10 +0700

    update home

--- application/views/home/homepage.php
+++ application/views/home/homepage.php
@@ -73,11 +73,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 </ul>
             </nav> <!-- end s-header__nav-wrap -->
 
-            <a href="mailto:#0" class="btn btn--primary btn--small">
+            <!-- <a href="mailto:#0" class="btn btn--primary btn--small">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z" /></svg>
                 Get In Touch
-            </a>
+            </a> -->
 
         </div> <!-- end header-content -->
 
@@ -120,7 +120,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
         </div>
 
         <div class="s-hero__video">
-            <a class="s-hero__video-link" href="https://player.vimeo.com/video/117310401?color=01aef0&amp;title=0&amp;byline=0&amp;portrait=0" data-lity="">
+            <a class="s-hero__video-link" href="https://www.youtube.com/watch?v=Vyw4AwWEV9A" data-lity="">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path d="M21 12l-18 12v-24z" /></svg>
                 <span class="s-hero__video-text">Play Video</span>
