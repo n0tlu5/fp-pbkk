907bf73631e0fce2ad4c3683ec757426ab3eab62
Darfi Sultoni <darfisultoni@gmail.com>
2020-05-21 09:47:34 +0700

    typo

--- application/views/admin/donasi/new_form.php
+++ application/views/admin/donasi/new_form.php
@@ -19,7 +19,7 @@
 				<div class="container-fluid">
 					<!-- Page Heading -->
 					<div class="d-sm-flex align-items-center justify-content-between mb-4">
-						<h1 class="h3 mb-0 text-gray-800">Add product</h1>
+						<h1 class="h3 mb-0 text-gray-800">Tambahkan donasi</h1>
 					</div>
 
 					<?php if ($this->session->flashdata('success')): ?>
